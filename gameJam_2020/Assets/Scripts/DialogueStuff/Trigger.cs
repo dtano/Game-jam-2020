d7b3e9e00ec853b799b7a4a91e5c64f787295c76
Hwijayas <wijayahans506@yahoo.com>
2020-08-17 23:19:48 +0700

    Added dialogue scripts

---
+++ gameJam_2020/Assets/Scripts/DialogueStuff/Trigger.cs
@@ -0,0 +1,15 @@
+﻿    using UnityEngine;
+    using System.Collections;
+     
+    public class Trigger : MonoBehaviour {
+     
+
+        void OnMouseDown()
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+
+        public Dialogue dialogue;
+
+        
+    }
