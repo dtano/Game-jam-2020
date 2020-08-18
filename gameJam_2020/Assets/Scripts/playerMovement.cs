6cfad7c2f2b9d53d8fa838c93a5e9c80aaeec7b1
Denzel Joaquin Tano <denzel_tano@yahoo.com>
2020-08-18 16:19:56 +1000

    Changed the movement script

--- gameJam_2020/Assets/Scripts/playerMovement.cs
+++ gameJam_2020/Assets/Scripts/playerMovement.cs
@@ -9,6 +9,8 @@ public class playerMovement : MonoBehaviour
     public Animator animator;
 
     private bool faceRight = true;
+
+    private Vector2 target;
     
     // Start is called before the first frame update
     void Start()
@@ -19,19 +21,41 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        // //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         float move = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(move, 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
+        // Vector3 movement = new Vector3(move, 0f, 0f);
+        // transform.position += movement * Time.deltaTime * moveSpeed;
 
-        animator.SetFloat("Horizontal", Mathf.Abs(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime ));
+        // animator.SetFloat("Horizontal", Mathf.Abs(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime ));
 
-        //Flip the character
-        if(move < 0 && faceRight){
-            flip();
-        }else if(move > 0 && !faceRight){
-            flip();
+        // //Flip the character
+        // if(move < 0 && faceRight){
+        //     flip();
+        // }else if(move > 0 && !faceRight){
+        //     flip();
+        // }
+
+        // Getting the coordinates of the mousePosition in game
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        if(Input.GetMouseButtonDown(0)){
+            target = new Vector2(mousePos.x, transform.position.y);
+            Debug.Log(target);
         }
+
+        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * moveSpeed);
+
+        //animator.SetFloat("Horizontal", Mathf.Abs(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime ));
+
+        // if(target != null){
+        //     if(Mathf.Sign(transform.position.x) != Mathf.Sign(target.x)){
+        //         Debug.Log("Target is in the opposite direction");
+        //         flip();
+        //     }else{
+        //         Debug.Log("Target is in the same direction");
+        //     }
+        // }
+
         
     }
 
