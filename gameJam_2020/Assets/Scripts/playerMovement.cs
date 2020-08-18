f52e203b13739c64c6dec9e7348f4219aba2f4cc
Denzel Joaquin Tano <denzel_tano@yahoo.com>
2020-08-18 20:11:25 +1000

    Resolved the movement issue

--- gameJam_2020/Assets/Scripts/playerMovement.cs
+++ gameJam_2020/Assets/Scripts/playerMovement.cs
@@ -6,23 +6,31 @@ public class playerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     
-    public Animator animator;
+    // To prevent the player sprite from moving on its own at the start 
+    private bool noMove = false;
+    
+    //public Animator animator;
 
     private bool faceRight = true;
 
     private Vector2 target;
+    // private Vector2 position;
+    // private Camera cam;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        // target = new Vector2(0.0f, 0.0f);
+        // position = gameObject.transform.position;
+
+        // cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {   
         // //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        float move = Input.GetAxis("Horizontal");
+        //float move = Input.GetAxis("Horizontal");
         // Vector3 movement = new Vector3(move, 0f, 0f);
         // transform.position += movement * Time.deltaTime * moveSpeed;
 
@@ -36,14 +44,22 @@ public class playerMovement : MonoBehaviour
         // }
 
         // Getting the coordinates of the mousePosition in game
+
+        // float step = moveSpeed * Time.deltaTime;
+        // transform.position = Vector2.MoveTowards(transform.position, target, step);
+        // Debug.Log(target);
+        
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         
         if(Input.GetMouseButtonDown(0)){
             target = new Vector2(mousePos.x, transform.position.y);
-            Debug.Log(target);
+            noMove = true;
         }
 
-        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * moveSpeed);
+        if(noMove == true){
+            transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * moveSpeed);
+        }
+        //transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * moveSpeed);
 
         //animator.SetFloat("Horizontal", Mathf.Abs(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime ));
 
@@ -63,6 +79,23 @@ public class playerMovement : MonoBehaviour
         faceRight = !faceRight;
         transform.Rotate(0f, 180f, 0f);
     }
+
+    // void OnGUI(){
+    //     Event currentEvent = Event.current;
+    //     Vector2 mousePos = new Vector2();
+    //     Vector2 point = new Vector2();
+
+    //     // compute where the mouse is in world space
+    //     mousePos.x = currentEvent.mousePosition.x;
+    //     mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+    //     point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0.0f));
+
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         // set the target to the mouse click location
+    //         target = point;
+    //     }
+    // }
 
     
 }
