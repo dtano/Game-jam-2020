bd896d47fc5469fae39f99ac0b53e134df894e56
Denzel Joaquin Tano <denzel_tano@yahoo.com>
2020-08-21 16:16:43 +1000

    When the level starts, the opening dialogue is started

---
+++ gameJam_2020/Assets/OfficeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficeController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject door;
+    public Dialogue dialogue;
+
+    private bool levelStart = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.GetComponent<playerMovement>().enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(levelStart){
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            levelStart = false;
+        }
+    }
+}
