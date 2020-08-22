bfe6cc7dfa6cafad4664fc77aa2b798329d0b8e0
Denzel Joaquin Tano <denzel_tano@yahoo.com>
2020-08-22 15:26:11 +1000

    Added a script that checks the name of the currently selected object

---
+++ gameJam_2020/Assets/Scripts/Links/ButtonDetector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonDetector : MonoBehaviour
+{
+    void Start(){
+
+    }
+
+    void Update(){
+
+    }
+    
+    public void getCurrentObject(){
+        Debug.Log(EventSystem.current.currentSelectedGameObject);
+    }
+}
